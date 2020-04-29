4f9dec9dd4d69e3ca08e3577f9c3a7081ffc0a06
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-04-29 01:57:52 -0400

    Visualizacion de init

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,38 @@
 @extends('layout')
 
 @section('content')
+<?php
+/*
+Dentro del php.ini tenemos las siguientes directivas junto con sus valores iniciales:
+
+display_errors = On
+register_globals = Off
+post_max_size = 8M
+*/
+
+echo 'display_errors = ' . ini_get('display_errors') . "<br>";
+echo 'register_globals = ' . ini_get('register_globals') . "<br>";
+echo 'post_max_size = ' . ini_get('post_max_size') . "<br>";
+echo 'post_max_size+1 = ' . ((int)ini_get('post_max_size')+1) . "<br>";
+echo 'post_max_size in bytes = ' . return_bytes(ini_get('post_max_size'));
+
+function return_bytes($val) {
+    $val = (int)trim($val);
+    $last = strtolower($val[strlen($val)-1]);
+    switch($last) {
+        // El modificador 'G' está disponble desde PHP 5.1.0
+        case 'g':
+            $val *= 1024;
+        case 'm':
+            $val *= 1024;
+        case 'k':
+            $val *= 1024;
+    }
+
+    return $val;
+}
+
+?>
 	<section class="posts container">
 	@if (isset($title))
 	<h3>{{ $title }}</h3>
