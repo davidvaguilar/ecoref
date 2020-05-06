c2a13af8b0b8b3f8cc67e24f5f1261f7e532c8ba
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-05-06 16:51:36 -0400

    Ingresado el correo y nombre

--- resources/views/emails/work-order.blade.php
+++ resources/views/emails/work-order.blade.php
@@ -23,7 +23,7 @@
                                             <tr>
                                                 <td class="content-cell" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
                                                     <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">Nueva orden de trabajo de {{ config('app.name') }}</h1>
-                                                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Se ha generado una nueva orden de trabajo a noche de {{ nombre }} </p>
+                                                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Se ha generado una nueva orden de trabajo a noche de {{ $nombre }} </p>
                                                     <div class="table" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
                                                         Se adjunta Orden de Trabajo en formato PDF.
                                                     </div>
