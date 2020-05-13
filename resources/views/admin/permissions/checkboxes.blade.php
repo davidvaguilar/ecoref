555129ab61f276d48a71cb3f549a29366d7a7a65
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-05-13 03:36:08 -0400

    Actualizado permisos

--- resources/views/admin/permissions/checkboxes.blade.php
+++ resources/views/admin/permissions/checkboxes.blade.php
@@ -1,7 +1,7 @@
 @foreach ($permissions as $key => $values)
     <div class="checkbox">
         <label>
-        <input name="permissions[]" type="checkbox" value="{{ $key }}" 
+        <input name="permissions[]" type="checkbox" value="{{ $key+1 }}" 
             {{ $model->permissions->contains($values->id) 
                     || collect(old('permission'))->contains($values->name) ? 'checked': ''}}>
             {{ $values->display_name }}
