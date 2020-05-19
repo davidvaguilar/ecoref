ff6c94e362243abdcdfbba07ef8b5afb966185cc
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-05-19 16:30:05 -0400

    nuev test 2

--- resources/views/emails/work-order.blade.php
+++ resources/views/emails/work-order.blade.php
@@ -1,1 +1,13 @@
 Se ha generado una nueva orden de trabajo. Se adjunta Orden de Trabajo en formato PDF.
+
+
+Si deseas cancelar tu suscripción a nuestro newsletter, haz click aquí
+
+
+<form id="null" action="?ui=html&amp;zy=c" method="post">
+    <input type="hidden" name="at" value="AF6bupMM7sILtfdpnXimkTGFSsun4kmxDA">
+        <font face=arial>Para utilizar Gmail en la vista estándar, tiene que estar habilitado JavaScript, pero parece que está inhabilitado o que no es compatible con tu navegador. Si quieres usar la vista estándar, cambia la configuración de tu navegador para habilitar JavaScript y 
+            <a href="">vuelve a intentarlo</a>.<p>Si prefieres utilizar la vista básica en HTML de Gmail, que no exige JavaScript, 
+            <input type="submit" value="haz clic aquí" class="submit_as_link">.</p>
+        </font>
+</form>
