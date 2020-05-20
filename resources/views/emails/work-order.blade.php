63f7fef77d90cf9809cf62da55cc182bd46d2ad2
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-05-20 10:04:08 -0400

    Detalles de Correo

--- resources/views/emails/work-order.blade.php
+++ resources/views/emails/work-order.blade.php
@@ -4,19 +4,18 @@
 Se adjunta Archivo en Pdf del Reporte
 
 Gracias,<br>
-{{ config('app.name') }}
+{{ config('app.name') }}.
 
 
 {{-- Footer --}}
-@slot('footer')
-    @component('mail::footer')
-    © {{ date('Y') }} {{ config('app.name') }}. Todos los derechos reservados.
+@component('mail::footer')
+© {{ date('Y') }} {{ config('app.name') }}. Todos los derechos reservados.
 
-    Si no deseas recibir más correos, puedes [modificar tus preferencias][unsubscribe].
+Si no deseas recibir más correos, puedes [modificar tus preferencias][unsubscribe].
 
-    [unsubscribe]: {{ url('/configuracion') }}
-    @endcomponent
-@endslot
+[unsubscribe]: {{ url('/configuracion') }}
+@endcomponent
+
 
 @endcomponent
 
