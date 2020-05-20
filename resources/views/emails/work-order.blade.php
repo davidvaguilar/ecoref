f1024947f698baf7d068bcd0a5625e352a266e02
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-05-20 09:48:55 -0400

    Nuevo correo conf

--- resources/views/emails/work-order.blade.php
+++ resources/views/emails/work-order.blade.php
@@ -6,7 +6,17 @@ Se adjunta Archivo en Pdf del Reporte
 Gracias,<br>
 {{ config('app.name') }}
 
-Si no desea recibir más información puede cancelar su suscripción en cualquier momento. <br>Cambie aquí la configuración del envio de correo: 
-    <a href="https://r.ashampoo.com/r.php?id=1596156&amp;ri=elea&amp;u=23746213&amp;lk=YQPtHbaW" name="m_-2373931488776779919_Dropout es" style="color:#7f7f7f;text-decoration:underline;font-weight:bold" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://r.ashampoo.com/r.php?id%3D1596156%26ri%3Delea%26u%3D23746213%26lk%3DYQPtHbaW&amp;source=gmail&amp;ust=1590065314746000&amp;usg=AFQjCNFI8Lq5JzQUyP_VJwqDCKk4vcpJ4A">Configurar boletín</a>
+
+{{-- Footer --}}
+@slot('footer')
+    @component('mail::footer')
+    © {{ date('Y') }} {{ config('app.name') }}. Todos los derechos reservados.
+
+    Si no deseas recibir más correos, puedes [modificar tus preferencias][unsubscribe].
+
+    [unsubscribe]: {{ url('/configuracion') }}
+    @endcomponent
+@endslot
+
 @endcomponent
 
