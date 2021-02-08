0840314ddef38edec17cf22a263aa1b7a8302899
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2021-02-08 09:39:26 -0300

    Agrege ruta login

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@
     $url = "https://ecoref.davidaguilar.cl/login";
     return Redirect::to($url);
   });
+
+  Route::get('login', function () {
+    $url = "https://ecoref.davidaguilar.cl/login";
+    return Redirect::to($url);
+  });
